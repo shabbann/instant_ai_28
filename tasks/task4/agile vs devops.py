04e14fa672683f77acd7d65ad3e68fe14f2b944e
shabbann <140034820+shabbann@users.noreply.github.com>
2023-08-15 15:14:47 +0300

    Add files via upload

---
+++ tasks/task4/agile vs devops.py
@@ -0,0 +1,21 @@
+'''
+    agile is a way or a mindset a team work with
+    it works to adapt to changes in the environment
+    it uses frameworks like
+    scrum:it uses sprints which take a fixed time from weeks to a month
+    it uses also daily scrums to meet and discuss problems
+    "The Scrum Lifecycle consists of a series of Sprints, where the end result is a potentially shippable product increment"
+    extreme programming: it uses pair programming and weekly cycles and quarterly cycle.
+     as an iteration to tell the stories(escribe what the product should do in terms meaningful to customers)
+    in conclusion agile is a itretive method to work in projects
+    it is better than waterfall method in adabting to the outside world and evolving
+'''
+'''
+    while devops bring developers team and operations team together in a streamline flow in infinte phases
+    first phase is the planing phase where the dev team puts a plan
+    second is the coding phase with several version on the rebo 
+    and then it get into build phase and go to testing once the code is passed all this phases
+    it passes to the dev team and ready to deployment and operate and continuously monitored 
+    and the feedback is sent back to the planing phase
+    and like that the cycle keep repeating to adapt the changes
+'''
