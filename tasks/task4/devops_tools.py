3ef809259dfbb1f7c388f03a36cbef925cb40a5c
shabbann <140034820+shabbann@users.noreply.github.com>
2023-08-15 16:26:35 +0300

    Add files via upload

---
+++ tasks/task4/devops_tools.py
@@ -0,0 +1,11 @@
+'''
+   at the very core of devops is pipeline to test and release
+    ci/cd tools is the most important tool in devops
+    the most popular one is jenkins
+    second is cloud platform to be deployment environment
+    third is packaging where it is done by docker
+    but docker made deployment much easier so alot of packaging is created and become hard to manage
+    that's when kubernetes came to scale and heal microservices
+    then monitoring and alerting come to add protection to the project
+    and for extra protection we add infrastructures if the servers got down
+'''
