92820e8c76360ac07774f86d58b7e9118b198825
shabbann <140034820+shabbann@users.noreply.github.com>
2023-08-16 23:34:51 +0300

    Add files via upload

---
+++ tasks/task5/ways of computer architecture.py
@@ -0,0 +1,28 @@
+'''
+    computer architecture refers to the end-to-end structure of a computer system
+    that determines how its components interact with each other in helping to execute the machine’s purpose
+    there is two types of it CISC(Complex instruction set computer) and RISC(reduced instruction set computer)
+'''
+'''
+    CISC processors have one processing unit, auxiliary memory, and a tiny register set 
+    containing hundreds of unique commands.
+    These processors execute a task with a single instruction, 
+    making a programmer’s work simpler since fewer lines of code are required to complete the operation
+    This method utilizes less memory but may need more time to execute instructions
+    and work faster for high level languages compilers 
+    and this means it is more expensive
+'''
+'''
+    while RISK contains fewer commands code for favor of register sets
+    this means more insructions needed for optaining the same program
+    but the instructions take less clock cycles 
+    and it uses more RAM 
+    and this means it's cheaper and easier to design
+'''
+'''
+    in the end there is no one is BTETER than the other 
+    they both have cons ad pros and this means 
+    it depends of what do you need them for
+    arm and idm uses RISK 
+    while x86 uses CISC
+'''
