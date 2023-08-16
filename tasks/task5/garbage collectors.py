c3856afd4898b850dc25485bd16451c1d5d00f76
shabbann <140034820+shabbann@users.noreply.github.com>
2023-08-17 00:27:02 +0300

    Add files via upload

---
+++ tasks/task5/garbage collectors.py
@@ -0,0 +1,7 @@
+"""
+    garbage collection is he process of cleaning up variables that are out of scope(maybe a function or a ptr that got dereferenced)
+    by traking how many pointer are on the variable if the number goes to zero it get deleted and so on
+    the most pop languages that uses auto garbage collectors are:java python c# and rust
+    while the one who don't are :c and c++ (mainly because there are typesafe)
+
+"""
